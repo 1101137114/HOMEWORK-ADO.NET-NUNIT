7273d8e045fceb6b8fb0930d068449c3335ec27d
yohsuan10126 <yohsuan10126@gmail.com>
2015-04-04 17:10:41 +0800

    re-upload

---
+++ ClassLibrary1/Cal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Cal
+    {
+        public enum CurrencySelect
+        {
+            JPY = 0,//日幣
+            TWD = 1//台幣
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Age"></param>
+        /// <param name="format"</param>
+        /// <returns></returns>
+        public int GetResult(int USD, CurrencySelect Select)
+        {
+            if (Select == CurrencySelect.JPY)
+                return (USD * 120);
+            else if (Select == CurrencySelect.TWD)
+                return (USD * 30);
+
+            return 0;
+        }
+    }
+}
---
+++ WebApplication1.Tests/Class3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1.Tests
+{
+    [TestFixture]
+    public class Class3
+    {
+        [Test]
+        public void Cal()
+        {
+            ClassLibrary.Cal cal = new ClassLibrary.Cal();
+            int inputUSD = 10;
+            int expect = 300;
+
+            Assert.AreEqual(expect, cal.GetResult(inputUSD, ClassLibrary.Cal.CurrencySelect.TWD));
+        }
+    }
+}
